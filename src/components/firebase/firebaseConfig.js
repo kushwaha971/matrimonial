e1d222f4edec612589837045afae93fc1eb2609e
Akash <kushwahaakash971@gmail.com>
2023-01-08 13:57:02 +0530

    Firebase API integration

--- src/components/firebase/firebaseConfig.js
+++ src/components/firebase/firebaseConfig.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADLB1letQD2wrqRdWr3grNxTaJA6306G4",
-  authDomain: "vtu-shaadi.firebaseapp.com",
-  projectId: "vtu-shaadi",
-  storageBucket: "vtu-shaadi.appspot.com",
-  messagingSenderId: "608949678338",
-  appId: "1:608949678338:web:b90df3a3b76ff82d04521b",
-  measurementId: "G-XG46ZNL1PT",
+  apiKey: "AIzaSyBryCGoAQiOpSIqZD4ICYq4SaBoS4obHwg",
+  authDomain: "vtu-shaadi-8e279.firebaseapp.com",
+  projectId: "vtu-shaadi-8e279",
+  storageBucket: "vtu-shaadi-8e279.appspot.com",
+  messagingSenderId: "554686195198",
+  appId: "1:554686195198:web:31047b0afbb6ecb04a8caf",
+  measurementId: "G-XPKW16JVB6",
 };
 
 const app = initializeApp(firebaseConfig);
-export  const auth = getAuth(app);
+export const auth = getAuth(app);
 export default app;
